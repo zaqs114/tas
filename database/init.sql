1960224a87c5b8a648ea6250bf9f2baeb5a08772
zaqs114 <zaqs114@gmail.com>
2018-10-08 11:11:58 +0200

    updated docker for mac, added database skeleton

--- database/init.sql
+++ database/init.sql
@@ -1,50 +1,91 @@
-
-create table chairs
+--skrypt wygenerowny przez datagrip
+create table users
 (
-	id serial not null
-		constraint chairs_pkey
-			primary key,
-	height integer not null,
-	width integer not null,
-	color varchar(64) not null,
-	name varchar(64) not null
-)
-;
+  login      varchar(30)       not null
+    constraint users_pkey
+    primary key,
+  password   varchar(30)       not null,
+  avatar     varchar(200),
+  admin_perm integer default 0 not null
+);
 
-create table tables
+create unique index users_login_uindex
+  on users (login);
+
+create table games
 (
-	id serial not null
-		constraint tables_pkey
-			primary key,
-	height integer not null,
-	width integer not null,
-	lenght integer not null,
-	shape varchar(64) not null,
-	name varchar(65) not null
-)
-;
+  gameid      integer      not null
+    constraint games_pkey
+    primary key,
+  icon        varchar(200),
+  title       varchar(30)  not null,
+  description varchar(500) not null,
+  launchdate  date         not null,
+  publisher   varchar(30)  not null,
+  screen      varchar(200)
+);
 
-create unique index tables_id_uindex
-	on tables (id)
-;
+create unique index games_gameid_uindex
+  on games (gameid);
 
-create table sets
+create table reviews
 (
-	id serial not null
-		constraint sets_pkey
-			primary key,
-	chair_id integer not null
-		constraint chair_id
-			references chairs,
-	table_id integer not null
-		constraint table_id
-			references tables,
-	name varchar(64) not null
-)
-;
+  reviewid integer       not null
+    constraint reviews_pkey
+    primary key,
+  title    varchar(120)  not null,
+  content  varchar(1000) not null,
+  rate     integer       not null,
+  userid   varchar(30)   not null
+    constraint reviews_users_login_fk
+    references users,
+  gameid   integer       not null
+    constraint reviews_games_gameid_fk
+    references games
+);
 
-create unique index sets_id_uindex
-	on sets (id)
-;
+create unique index reviews_reviewid_uindex
+  on reviews (reviewid);
 
+create table platforms
+(
+  platformid    integer     not null
+    constraint platfroms_pkey
+    primary key,
+  platform_name varchar(30) not null
+);
+
+create unique index platfroms_platformid_uindex
+  on platforms (platformid);
+
+create table game_platform
+(
+  gameid     integer
+    constraint game_platform_games_gameid_fk
+    references games,
+  platformid integer
+    constraint game_platform_platforms_platformid_fk
+    references platforms
+);
+
+create table genre
+(
+  genreid    integer not null
+    constraint genre_pkey
+    primary key,
+  genre_name varchar(30)
+);
+
+create unique index genre_genreid_uindex
+  on genre (genreid);
+
+create table game_genre
+(
+  genreid integer
+    constraint game_genre_genre_genreid_fk
+    references genre,
+  gameid  integer
+    constraint game_genre_games_gameid_fk
+    references games
+);
 
