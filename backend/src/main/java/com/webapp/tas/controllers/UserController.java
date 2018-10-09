e3b923e6bcea3c966fd9e9e8d5e685c43a5ffade
zaqs114 <zaqs114@gmail.com>
2018-10-09 14:11:32 +0200

    added user registration

--- backend/src/main/java/com/webapp/tas/controllers/UserController.java
+++ backend/src/main/java/com/webapp/tas/controllers/UserController.java
@@ -1,23 +1,39 @@
 package com.webapp.tas.controllers;
 
 import com.webapp.tas.objects.User;
+import com.webapp.tas.tables.records.UsersRecord;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.webapp.tas.tables.Users.USERS;
+import static com.webapp.tas.Tables.USERS;
+
 
 @RestController
+@CrossOrigin(origins = "http://localhost:63342")
 public class UserController {
 
     @Autowired
     private DSLContext jooq;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/users")
-    public List<User> users() {
+    public List<User> usersList() {
         return jooq.select().from(USERS).fetchInto(User.class);
+    }
+
+    @PostMapping("/register")
+    public void registerUser(@RequestBody User user) {
+        UsersRecord ur = jooq.newRecord(USERS);
+        ur.setAvatar(user.getAvatar());
+        ur.setLogin(user.getLogin());
+        ur.setPassword(passwordEncoder.encode(user.getPassword()));
+        ur.setAdminPerm(0);
+        ur.store();
     }
 }
