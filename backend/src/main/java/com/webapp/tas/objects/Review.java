f011fec805e92da73ca8369fb038bae21e53ad4b
reznot <Reznot24mj.invincible@gmail.com>
2018-11-14 18:41:19 +0100

    minor fixes

--- backend/src/main/java/com/webapp/tas/objects/Review.java
+++ backend/src/main/java/com/webapp/tas/objects/Review.java
@@ -9,4 +9,27 @@ public @Data class Review {
     int rate;
     String userID;
     int gameID;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserid(String userID) {
+        this.userID = userID;
+    }
+
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewid(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+
+    public void setGameid(int gameID) {
+        this.gameID = gameID;
+    }
+
+
 }
